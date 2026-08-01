57c5c944a90c54b7cc046aafd23ae2106c49174e
uddipta089 <uddiptapathak0831@gmail.com>
2026-08-02 04:31:25 +0530

    Update RootLayout.jsx

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import { LogOut, HeartPulse } from 'lucide-react';
 
 const RootLayout = () => {
