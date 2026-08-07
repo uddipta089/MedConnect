66a4b348428da70f8e926971ce82069edebade20
uddipta089 <uddiptapathak0831@gmail.com>
2026-08-07 19:30:29 +0530

    Delete server directory

--- server/middleware/role.js
+++
@@ -1,9 +0,0 @@
-export const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      res.status(403);
-      throw new Error(`User role ${req.user.role} is not authorized to access this route`);
-    }
-    next();
-  };
-};
--- server/middleware/validate.js
+++
@@ -1,8 +0,0 @@
-export const validate = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.body);
-    next();
-  } catch (error) {
-    next(error); // Pass Zod error to global error handler
-  }
-};
