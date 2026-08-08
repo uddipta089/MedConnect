0eec5b04efd15f687c1c7a284a3e2b141d77c549
uddipta089 <uddiptapathak0831@gmail.com>
2026-08-08 20:04:52 +0530

    Update jwtHelper.js

--- server/utils/jwtHelper.js
+++ server/utils/jwtHelper.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (id, email, role) => {
   return jwt.sign({ id, email, role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '15m',
+    expiresIn: process.env.JWT_EXPIRES_IN || '30d',
   });
 };
 
